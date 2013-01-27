a3331b80365ab68b238900bd69861a30fc05c1d3
Gregory Liras <gregliras@gmail.com>
2013-01-27 17:58:15 +0200

    updated script and all

--- src/results/Slides/files/parse_results.sh
+++ src/results/Slides/files/parse_results.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+
+function plotot {
+    output=$1
+    machine=$2
+    in_t=$3
+    in_sp=$4
 gnuplot << EOF
     set terminal png size 1024, 768
-    set output "sandman_tiled_cilk_for.png"
-    set title "sandman times"
+    set output "${output}"
+    set title "${machine} times"
     set xlabel "cores"
     set boxwidth 0.5
     set grid
@@ -13,12 +19,15 @@ gnuplot << EOF
     set multiplot layout 1,2
     set ylabel "Time"
     set title "Time"
-    plot "sandman_tiled_cilk_for.res" lt 3 title "Times" with boxes
+    plot "${in_t}" lt 3 title "Times" with boxes
     set ylabel "Speedup"
     set title "Speedup"
     set yrange[0:25]
-    plot "sandman_tiled_cilk_for_speedup.res" lt 4 title "Speedup" with lines, \
-        "sandman_tiled_cilk_for_speedup.res" lt 4 notitle with points
+    plot "${in_sp}" lt 4 title "Speedup" with lines, \
+        "${in_sp}" lt 4 notitle with points
     unset multiplot
     set key 
 EOF
+}
+
+plotot "sandman_tiled_cilk_for.png" "sandman" "sandman_tiled_cilk_for.res" "sandman_tiled_cilk_for_speedup.res"
