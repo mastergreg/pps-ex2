4c8448a29c1c7b396ecef1cf8637361cabdb918d
Gregory Liras <gregliras@gmail.com>
2013-01-29 12:51:53 +0200

    sync

--- src/results/Slides/files/res/final_plot/parse_results.sh
+++ src/results/Slides/files/res/final_plot/parse_results.sh
@@ -8,6 +8,9 @@ function plotot {
     rec_cp_spd=$5
     tiled_rcp_spd=$6
     tiled_cfcp_spd=$7
+    obv_t=$8
+    obv_s=$8
+
 gnuplot << EOF
     set terminal png size 1024, 768
     set output "${output}"
@@ -22,20 +25,23 @@ gnuplot << EOF
     set multiplot layout 1,2
     set ylabel "Time"
     set title "Time"
-    plot    "${rec_cp}" using 2:xtic(1) lt 3 title "rec gcc-cilkplus" with lines, \
-         "${tiled_rcp}" using 2:xtic(1) lt 4 title "tiled rec_for gcc-cilkplus" with lines, \
-        "${tiled_cfcp}" using 2:xtic(1) lt 5 title "tiled cilk_for gcc-cilkplus" with lines
+    plot    "${rec_cp}" using 2:xtic(1) lt 3 title "rec" with lines, \
+         "${tiled_rcp}" using 2:xtic(1) lt 4 title "tiled rec_for" with lines, \
+        "${tiled_cfcp}" using 2:xtic(1) lt 6 title "tiled cilk_for" with lines, \
+        "${obv_t}" using 2:xtic(1) lt 7 title "simple implementation" with lines
     unset ylabel
 
     set ylabel "Speedup"
     set title "Speedup"
     set yrange[0:*]
     plot    "${rec_cp_spd}" using 2:xtic(1) lt 3 notitle with points, \
-            "${rec_cp_spd}" using 2:xtic(1) lt 3 title "rec gcc-cilkplus" with lines, \
+            "${rec_cp_spd}" using 2:xtic(1) lt 3 title "rec" with lines, \
          "${tiled_rcp_spd}" using 2:xtic(1) lt 4 notitle with points, \
-         "${tiled_rcp_spd}" using 2:xtic(1) lt 4 title "tiled rec_for gcc-cilkplus" with lines, \
-        "${tiled_cfcp_spd}" using 2:xtic(1) lt 5 notitle with points, \
-        "${tiled_cfcp_spd}" using 2:xtic(1) lt 5 title "tiled cilk_for gcc-cilkplus" with lines
+         "${tiled_rcp_spd}" using 2:xtic(1) lt 4 title "tiled rec_for" with lines, \
+        "${tiled_cfcp_spd}" using 2:xtic(1) lt 6 notitle with points, \
+        "${tiled_cfcp_spd}" using 2:xtic(1) lt 6 title "tiled cilk_for" with lines, \
+        "${obv_s}" using 3:xtic(1) lt 7 notitle with points, \
+        "${obv_s}" using 3:xtic(1) lt 7 title "simple implementation" with lines
     unset multiplot
     set key 
 EOF
@@ -56,9 +62,14 @@ EOF
 #sandman_rec_time.res sandman_tiled_cilk_for.res sandman_tiled_cilkplus_rec_for_sweep.res
 #sandman_rec_speedup.res sandman_tiled_cilk_for_speedup.res sandman_tiled_cilkplus_rec_for_sweep_speedup.res
 
-plotot "final_plot.png" sandman_rec_time.res sandman_tiled_cilk_for.res sandman_tiled_cilkplus_rec_for_sweep.res sandman_rec_speedup.res sandman_tiled_cilk_for_speedup.res sandman_tiled_cilkplus_rec_for_sweep_speedup.res
-
-
+plotot "final_plot.png" \
+sandman_rec_cilkplus_time.res \
+sandman_tiled_cilkplus_rec_for_sweep.res \
+sandman_tiled_cilk_for.res \
+sandman_rec_cilkplus_speedup.res \
+sandman_tiled_cilkplus_rec_for_sweep_speedup.res \
+sandman_tiled_cilk_for_speedup.res \
+sandman_capt_obv.res
 #clones_tiled_serial_rec_for_pbs.res clones_tiled_mit_rec_for_pbs.res clones_tiled_cilkplus_rec_for_pbs.res clones_tiled_mit_rec_for_pbs_speedup.res clones_tiled_cilkplus_rec_for_pbs_speedup.res
 
 
